44b7bd500d27ca239299b433844f4e738c349551
David Demers <e.ddemers3@etu.cvm.qc.ca>
2022-02-07 18:14:13 -0500

    afficher partie

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ class Vue:
     def creer_interface(self):
         self.cadre_info = Canvas(self.root, bg="darkgreen", height=self.modele.hauteur,
                                  width=self.modele.largeur)
-        self.cadre_info.pack()
-        self.afficher_partie()
         self.cadre_info.create_rectangle(0, self.modele.hauteur / 2 - 50, self.modele.largeur,
                                          self.modele.hauteur / 2 + 50, fill="beige")
+        self.cadre_info.pack()
+        self.afficher_partie()
 
     def afficher_partie(self):
         for i in self.modele.monstreList:
@@ -45,10 +45,20 @@ class Modele:
 
 class Controleur:
     def __init__(self):
+        self.partie_en_cours = 0
         self.modele = Modele(self)
         self.vue = Vue(self)
         self.vue.root.mainloop()
+
+    def debuter_partie(self):
+        self.partie_en_cours = 1
         self.modele.creer_monstres()
+        self.jouer_partie()
+
+    def jouer_partie(self):
+        if self.partie_en_cours:
+            self.vue.root.after(40, self.jouer_partie)
+        self.vue.afficher_partie()
 
 
 if __name__ == '__main__':
