73f1d92d5a2060ce729ae040a3ee7a5438c871c3
Jean-Christophe Caron <Caronjc00@gmail.com>
2022-02-07 16:51:11 -0500

    ajouter la classe monstre

---
+++ monstre.py
@@ -0,0 +1,17 @@
+
+class Monstre():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.vitesse= 3
+
+    def avancer_monstre(self,cibleX,cibleY):
+        #cibleX va représenter le premier composant du vecteur dans le tablau du chemin
+        #cibleX va représenter le deuxième composant du vecteur dans le tablau du chemin
+        if self.x < cibleX - self.vitesse:
+            self.x = self.x + self.vitesse
+
+        if self.y < cibleY - self.vitesse:
+            self.y = self.y + self.vitesse
+
+
