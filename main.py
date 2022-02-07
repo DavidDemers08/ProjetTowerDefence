8c619b4a4049b247b5eb6c073eadcbf70c044411
David Demers <e.ddemers3@etu.cvm.qc.ca>
2022-02-07 17:05:25 -0500

    mise a jour

--- main.py
+++ main.py
@@ -4,20 +4,37 @@ from tkinter import *
 class Vue():
     def __init__(self, parent):
         self.parent = parent
+        self.modele = self.parent.modele
         self.root = Tk()
-        self.root.title("JMDefence 0.1")
+        self.root.title("TowerDefence 0.1")
+        self.creer_interface()
+
+    def creer_interface(self):
+        self.cadre_info = Canvas(self.root, bg="darkgreen", height=self.modele.hauteur,
+                                 width=self.modele.largeur)
+
+        self.cadre_info.pack()
+        self.afficher_partie()
+
+
+    def afficher_partie(self):
+        self.cadre_info.create_rectangle(0, self.modele.hauteur / 2 - 50, self.modele.largeur,
+                                         self.modele.hauteur / 2 + 50, fill="beige")
 
 
 class Modele():
     def __init__(self, parent):
         self.parent = parent
+        self.largeur = 1000
+        self.hauteur = 800
 
 
 class Controleur():
     def __init__(self):
-        self.vue = Vue(self)
         self.modele = Modele(self)
+        self.vue = Vue(self)
         self.vue.root.mainloop()
+
 
 
 if __name__ == '__main__':
--- monstre.py
+++ monstre.py
@@ -1,17 +1,14 @@
-
 class Monstre():
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.vitesse= 3
+        self.vitesse = 3
 
-    def avancer_monstre(self,cibleX,cibleY):
-        #cibleX va représenter le premier composant du vecteur dans le tablau du chemin
-        #cibleX va représenter le deuxième composant du vecteur dans le tablau du chemin
+    def avancer_monstre(self, cibleX, cibleY):
+        # cibleX va représenter le premier composant du vecteur dans le tablau du chemin
+        # cibleX va représenter le deuxième composant du vecteur dans le tablau du chemin
         if self.x < cibleX - self.vitesse:
             self.x = self.x + self.vitesse
 
         if self.y < cibleY - self.vitesse:
             self.y = self.y + self.vitesse
-
-
