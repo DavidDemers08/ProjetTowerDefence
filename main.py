1db648fecd0beadedccac37739cdd6b1b2a545e8
DavidDemers08 <95939426+DavidDemers08@users.noreply.github.com>
2022-02-07 16:06:21 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+from tkinter import *
+
+class Vue():
+    def __init__(self,parent):
+        self.parent = parent
+        self.root = Tk()
+        self.root.title("TowerDefence 0.1")
+
+class Modele():
+    def __init__(self,parent):
+        self.parent = parent
+
+
+class Controleur():
+    def __init__(self):
+        self.vue = Vue(self)
+        self.modele = Modele(self)
+        self.vue.root.mainloop()
+
+
 
 if __name__ == '__main__':
-    print("salut")
+    c = Controleur()
+    print("Fin du programme")
