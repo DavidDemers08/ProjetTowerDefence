ec740867125e96e0e3f0c328babe745a9b85b9a8
David Demers <e.ddemers3@etu.cvm.qc.ca>
2022-02-07 19:31:59 -0500

    code corrigé

--- main.py
+++ main.py
@@ -1,66 +1,89 @@
 import random
 from tkinter import *
 
-from monstre import Monstre
-
 
 class Vue:
     def __init__(self, parent):
         self.parent = parent
+
         self.modele = self.parent.modele
         self.root = Tk()
-        self.root.title("TowerDefence 0.1")
+        self.root.title("TowerDefence, alpha_0.1")
         self.creer_interface()
 
     def creer_interface(self):
-        self.cadre_info = Canvas(self.root, bg="darkgreen", height=self.modele.hauteur,
-                                 width=self.modele.largeur)
-        self.cadre_info.create_rectangle(0, self.modele.hauteur / 2 - 50, self.modele.largeur,
-                                         self.modele.hauteur / 2 + 50, fill="beige")
-        self.cadre_info.pack()
+        # cadre HUD affichant la duree
+        cadre_depart = Frame(self.root, bg='gray')
+        bouton_depart = Button(cadre_depart, text='Commencer la partie', command=self.parent.debuter_partie)
+        self.canevas = Canvas(self.root, width=self.modele.largeur_carte, height=self.modele.hauteur_carte,
+                              bg="darkgreen")
+
+        cadre_depart.pack(expand=True, fill=BOTH)
+        bouton_depart.pack()
+        self.canevas.pack()
+
         self.afficher_partie()
 
     def afficher_partie(self):
-        for i in self.modele.monstreList:
-            self.cadre_info.create_oval(i.x - 5, i.y - 5, i.x + 5, i.y + 5, fill="red")
+        self.canevas.delete(ALL)
+
+        self.canevas.create_rectangle(0, self.modele.hauteur_carte / 2 - 50, self.modele.largeur_carte,
+                                      self.modele.hauteur_carte / 2 + 50, fill="beige")
+
+        for i in self.modele.liste_monstres:
+            self.canevas.create_oval(i.x - 5, i.y - 5, i.x + 5, i.y + 5, fill="black")
 
 
 class Modele:
     def __init__(self, parent):
         self.parent = parent
-        self.largeur = 1000
-        self.hauteur = 800
-        self.chemin = [(self.largeur, self.hauteur / 2)]  # tableau de chemin (ici juste la ligne droite)
-        self.monstreList = []
-        self.creer_monstres()
+        self.largeur_carte = 800
+        self.hauteur_carte = 800
+        self.vague = 0
+        self.liste_monstres = []
 
-    def avancer_monstres(self):
-        for i in self.monstreList:
-            i.avancer_monstre(1000, 400)
+    def creer_monstre(self):
 
-    def creer_monstres(self):
-        for i in range(51):
-            self.monstreList.append(Monstre(-10, random.randrange(350, 450)))
+        for i in range(10):
+            self.liste_monstres.append(Monstre(0, random.randrange(350, 450)))
+
+    def bouger_monstres(self):
+        for i in self.liste_monstres:
+            i.avancer_monstre()
+
+
+class Monstre:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.vitesse = 3
+
+    def avancer_monstre(self):
+        self.x += self.vitesse
 
 
 class Controleur:
     def __init__(self):
         self.partie_en_cours = 0
+
         self.modele = Modele(self)
         self.vue = Vue(self)
         self.vue.root.mainloop()
 
     def debuter_partie(self):
-        self.partie_en_cours = 1
-        self.modele.creer_monstres()
-        self.jouer_partie()
+        if not self.partie_en_cours:
+            self.partie_en_cours = 1
+            self.jouer_partie()
+            self.modele.vague = 1
+            self.modele.creer_monstre()
 
     def jouer_partie(self):
         if self.partie_en_cours:
             self.vue.root.after(40, self.jouer_partie)
+            self.modele.bouger_monstres()
         self.vue.afficher_partie()
 
 
 if __name__ == '__main__':
     c = Controleur()
-    print("Fin du programme")
+    print("L'application se termine")
