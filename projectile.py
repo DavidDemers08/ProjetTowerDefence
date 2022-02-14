69152d967f0825a772712482227a650a41e79171
Samuel Horvath <golfjob565@gmail.com>
2022-02-14 14:04:32 -0500

    class Projectile

---
+++ projectile.py
@@ -0,0 +1,24 @@
+class projectile:
+    def __init__(self,tour,monstre):
+        self.x=tour.x
+        self.y =tour.y
+        self.cibleX = monstre.x
+        self.cibleY = monstre.y
+        self.vitesse = 20
+
+    def lancer_projectile(self):
+
+            if self.x < self.cibleX:
+                self.x = self.x + self.vitesse
+            elif self.x > self.cibleX:
+                self.x -= self.vitesse
+
+            if self.y < self.cibleY:
+                self.y += self.vitesse
+            elif self.y > self.cibleY:
+                self.y -= self.vitesse
+
+            if self.y == self.cibleY and self.x == self.cibleX:
+                self.x = self.cibleX
+                self.y = self.cibleY
+
