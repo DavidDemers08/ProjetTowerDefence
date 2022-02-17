2e345e8788a68350a92f1f20689b373c2843fbda
Caron <Caronjc00@gmail.com>
2022-02-17 14:48:53 -0500

    classe Boss

--- boss.py
+++ boss.py
@@ -1,7 +1,9 @@
 import monstre
 
+
 class Boss(monstre.Monstre):
-    def __init__(self, x, y):
-        super().__init__(x, y)
+    def __init__(self, x, y, vitesse, vie):
+        super().__init__(x, y, vitesse, vie)
 
-
+    def avancer_monstre(self, path):
+        super().avancer_monstre(path)
