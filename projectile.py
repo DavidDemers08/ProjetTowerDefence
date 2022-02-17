12e8069cfa90a45daaf0da7b23ff135fe291af38
Caron <Caronjc00@gmail.com>
2022-02-17 15:34:51 -0500

    ajustement tour

--- projectile.py
+++ projectile.py
@@ -3,16 +3,15 @@ class Projectile:
     def __init__(self,tour,monstre):
         self.x= tour.x
         self.y =tour.y
-        self.cibleX = monstre.x
-        self.cibleY = monstre.y
-        self.vitesse = 10
+        self.vitesse = 20
         self.monstre = monstre
+        self.delai = 0
 
     def lancer_projectile(self):
-
-        distance = helper.Helper.calcDistance(self.x, self.y, self.cibleX, self.cibleY)
+        self.delai += 1
+        distance = helper.Helper.calcDistance(self.x, self.y, self.monstre.x,self.monstre.y)
         if distance > 0:
-            angle = helper.Helper.calcAngle(self.x, self.y, self.cibleX, self.cibleY)
+            angle = helper.Helper.calcAngle(self.x, self.y,self.monstre.x,self.monstre.y)
             cible = helper.Helper.getAngledPoint(angle,self.vitesse,self.x,self.y)
             self.x = cible[0]
             self.y = cible[1]
